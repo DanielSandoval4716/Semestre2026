03caa398e196950a21dfe9ec6331eb0f9bd42268
DanielSandoval4716 <dsandovalt1@gmail.com>
2026-03-10 23:01:20 -0600

    Ses3_P2

---
+++ Ses3_P2/Desafio1/Program.cs
@@ -0,0 +1,74 @@
+﻿// Crea un sistema de inicio de sesión de usuario, donde el usuario puede primero registrarse y luego iniciar sesión. El programa debe verificar si el usuario ha ingresado el nombre de usuario y la contraseña correctos al iniciar sesión (por lo tanto, los mismos que utilizó al registrarse).
+//
+//
+// Utiliza declaraciones If, ingresos del usuario y métodos para resolver el desafío.
+class persoina
+{
+    public string nombre;
+    public string contra;
+    public persoina(string nombre, string contra)
+    {
+        this.nombre = nombre;
+        this.contra = contra;
+        Console.WriteLine("usuario creado ");
+    }
+}
+
+class principal
+{
+    static void Main()
+    {
+        persoina[] persoinas = new persoina[20];
+        int ct = 0;
+        int n = 3;
+        do
+        {
+            Console.WriteLine(
+            "menu\n" +
+            "---------------------\n" +
+            "opcion 1: Ingresar nuevo usuario\n" +
+            "opcion 2: Iniciar sesion\n" +
+            "opcion 3: Salir"
+            );
+            n = int.Parse(Console.ReadLine());
+            if (n == 3)
+            {
+                Console.WriteLine("Haz salido del menu");
+                break;
+            }
+            switch (n)
+            {
+                case 1:
+                    Console.WriteLine("ingrese el nombre del usuario: ");
+                    string nm = Console.ReadLine();
+                    Console.WriteLine("ingrese la contraseña: ");
+                    string ctr = Console.ReadLine();
+                    persoinas[ct] = new persoina(nm, ctr);
+                    ct++;
+                    break;
+                case 2:
+                    Console.WriteLine("ingrese el nombre del usuario: ");
+                    string nm1 = Console.ReadLine();
+                    Console.WriteLine("ingrese la contraseña: ");
+                    string ctr1 = Console.ReadLine();
+                    bool buscador = false;
+                    for (int i = 0; i < ct; i++)
+                    {
+                        if (persoinas[i].nombre== nm1 && persoinas[i].contra == ctr1)
+                        {
+                          buscador = true;
+                          break;
+                        }
+                    }
+                    if (buscador == true)
+                    {
+                      Console.WriteLine("El usuario es correcto, se ha encontrado");
+                    }else
+                    {
+                        Console.WriteLine("El usuario no existe");
+                    }
+                    break;
+            }
+        } while (true);
+    }
+}
---
+++ Ses3_P2/Desafio2/Program.cs
@@ -0,0 +1,68 @@
+﻿// Crea una aplicación que maneje los puntajes de un juego imaginario. Tendrá un puntaje, un puntaje record y un jugador record.
+//
+//
+// Crea un método que tenga dos parámetros, uno para el puntaje obtenido y otro para el nombre del jugador.
+//
+//
+// Cuando se llama a ese método, se debe verificar si el puntaje del jugador es mayor que la puntuación más alta, si es así, que se escriba en la consola algo así como"La nueva puntuación más alta es " + puntuación. Y en otra línea "La puntuación más alta fue lograda por " + nombre del jugador.
+//
+//
+// Si no fue así, entonces que se escriba algo como "La puntuación más alta de " + puntuación más alta + " no se ha podido superar, y aún está en manos de " + jugador record.
+//
+//
+// Considera qué variables se requieren globalmente y cuáles localmente.
+class jugadores
+{
+    public string nombre;
+    public int puntaje;
+    public jugadores(string nm, int punt)
+    {
+        this.nombre = nm;
+        this.puntaje = punt;
+    }
+}
+class programa
+{
+    static void Main()
+    {
+        jugadores[] jugador = new jugadores[2];
+        jugador[0] = new jugadores("Jugador1", 0);
+        int n = 2;
+        do
+        {
+            Console.WriteLine(
+            "menu\n" +
+            "---------------------\n" +
+            "opcion 1: Ingresar jugador y puntaje\n" +
+            "opcion 2: ver punteo mayor\n" +
+            "opcion 3: Salir"
+            );
+            n = int.Parse(Console.ReadLine());
+            if (n == 3)
+            {
+                break;
+            }
+            else if (n == 1)
+            {
+                Console.WriteLine("ingrese el nombre del jugador: ");
+                string nm = Console.ReadLine();
+                Console.WriteLine("ingrese el puntaje del jugador: ");
+                int punta = int.Parse(Console.ReadLine());
+                jugador[1] = new jugadores(nm, punta);
+                if (jugador[0].puntaje > jugador[1].puntaje)
+                {
+                    Console.WriteLine("El jugador " + jugador[0].nombre + " mantiene la puntacion mayor con " + jugador[0].puntaje + " puntos");
+                }
+                else
+                {
+                    Console.WriteLine("El jugador " + jugador[1].nombre + " ha superado el puntaje record de " + jugador[0].puntaje + " con " + jugador[1].puntaje + " puntos");
+                    jugador[0] = jugador[1];
+                }
+            }
+            else if (n == 2)
+            {
+                Console.WriteLine("El jugador " + jugador[0].nombre + " es el jugador com mas puntos con " + jugador[0].puntaje + " puntos");
+            }
+        } while (true);
+    }
+}
